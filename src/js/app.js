0f28c9c72bb2f425fcd5ceab984ca1043b0729bc
VladVorobey <vladankaria@gmail.com>
2018-08-27 16:40:16 +0300

    google maps

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,37 @@
 import $ from 'jquery';
-import slick-carusel from 'slick-carousel';
+import slick from 'slick-carousel';
 
-$(function() {
-  $(document).ready(function(){
-    $('.your-class').slick({
-      setting-name: setting-value
+$(document).ready(function() {
+	 $('.slider-for').slick({
+	  slidesToShow: 1,
+	  slidesToScroll: 1,
+	  arrows: true,
+	  // fade: true,
+	  asNavFor: '.slider-nav'
   });
+  $('.slider-nav').slick({
+	  slidesToShow: 1,
+	  slidesToScroll: 1,
+	  asNavFor: '.slider-for',
+	  dots: false,
+	  vertical: true,
+	  centerMode: true,
+    centerPadding: '179px',
+	  focusOnSelect: true,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 5000
   });
-};
+  $('.reviews__slider').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: true,
+    autoplay: true,
+    autoplaySpeed: 2000
+  });
+  
+});
+
+
+
 
