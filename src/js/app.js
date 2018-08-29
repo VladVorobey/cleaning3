85e8ffd120c6214f329a5a4ddc4c9dd85fcedd1c
VladVorobey <vladankaria@gmail.com>
2018-08-29 10:38:43 +0300

    modals

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,8 @@
 import $ from 'jquery';
 import slick from 'slick-carousel';
+import magnificPopup from 'magnific-popup';
 
+//------------- GOOGLE MAP ----------
 var map;
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
@@ -16,7 +18,7 @@ function initMap() {
   });
 }
 initMap();
-
+//---------- SLIDER ---------------
 $(document).ready(function() {
 	 $('.slider-for').slick({
 	  slidesToShow: 1,
@@ -36,18 +38,92 @@ $(document).ready(function() {
 	  focusOnSelect: true,
     arrows: false,
     autoplay: true,
-    autoplaySpeed: 5000
+    autoplaySpeed: 3000
   });
   $('.reviews__slider').slick({
     slidesToShow: 3,
     slidesToScroll: 1,
     arrows: true,
     autoplay: true,
-    autoplaySpeed: 2000
+    autoplaySpeed: 5000
   });
-  
-});
+  //------- FIRST SCREEN SLIDER -------------
+  $('.first-screen__nav').slick({
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    arrows: true,
+    focusOnSelect: true,
+    vertical: true,
+    centerMode: true,
+    // fade: true,
+    asNavFor: '.first-screen__elem',
+  });
+  $('.first-screen__elem').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    asNavFor: '.first-screen__nav',
+    dots: false,
+    focusOnSelect: true,
+    arrows: false,
+    // autoplay: true,
+    // autoplaySpeed: 5000
+  });
 
+  //--------- ANCHOR --------------------
+  $('.nav a[href^="#"]').on('click', function(event) {
+
+    var target = $(this.getAttribute('href'));
+
+    if( target.length ) {
+      event.preventDefault();
+      $('html, body').stop().animate({
+        scrollTop: target.offset().top
+      }, 1000);
+    }
+
+  });
+
+  //-------- PRELOADER ------------------
+  $(window).on('load', function() {
+    $('.preloader').delay(1000).fadeOut('slow');
+  });
+  //--------------- MODAL -------------------
+  $(function() {
+    $('.popup-modal').magnificPopup({
+      type: 'inline',
+      preloader: false,
+      focus: '#username',
+      modal: true
+    });
+    $(document).on('click', '.popup-modal-dismiss', function(e) {
+      e.preventDefault();
+      $.magnificPopup.close();
+    });
+  });
+
+  //----- menu ------
+  $('.menu__btn, nav').click(function() {
+    $('header').toggleClass('active'),
+    $('.menu__btn').toggleClass('active');
+  });
+
+  //--------- E-mail Ajax Send
+  $('form').submit(function() { //Change
+    var th = $(this);
+    $.ajax({
+      type: 'POST',
+      url: 'sendmail.php', //Change
+      data: th.serialize()
+    }).done(function() {
+      alert('Спасибо за заявку!');
+      setTimeout(function() {
+        // Done Functions
+        th.trigger('reset');
+      }, 1000);
+    });
+    return false;
+  });
+});
 
 
 
