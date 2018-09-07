a63cf47168cd443e0ac6f9593d5ea3438204a70f
VladVorobey <vladankaria@gmail.com>
2018-09-07 12:16:10 +0300

    form work+ ajax work - validation

--- src/js/app.js
+++ src/js/app.js
@@ -208,14 +208,15 @@ $(document).ready(function() {
   });
 
   //--------- E-mail Ajax Send
-  $('form').submit(function() { //Change
+  $('.form').submit(function() { //Change
     var th = $(this);
     $.ajax({
       type: 'POST',
       url: 'sendmail.php', //Change
       data: th.serialize()
     }).done(function() {
-      alert('Спасибо за заявку!');
+      $('.modal').popup('hide');
+      $('#thanks').popup('show');
       setTimeout(function() {
         // Done Functions
         th.trigger('reset');
