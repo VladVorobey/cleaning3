47236aca87fef38ce78df58520f03d267312262a
VladVorobey <vladankaria@gmail.com>
2018-09-07 11:42:17 +0300

    test form +add popup overlay

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,9 @@
 import $ from 'jquery';
 import slick from 'slick-carousel';
 import magnificPopup from 'magnific-popup';
-import '../../node_modules/jquery-validation/dist/jquery.validate.min';
 import '../../node_modules/jquery-popup-overlay/jquery.popupoverlay';
-import '../../node_modules/jquery-mask-plugin/dist/jquery.mask.min';
-//------------- GOOGLE MAP ----------
 
+//------------- GOOGLE MAP ----------
 var map;
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
@@ -210,53 +208,21 @@ $(document).ready(function() {
   });
 
   //--------- E-mail Ajax Send
-  $('input[type="tel"]').mask('+0 (000) 000-00-00');
-
-  jQuery.validator.addMethod('phoneno', function(phone_number, element) {
-    return this.optional(element) || phone_number.match(/\+[0-9]{1}\s\([0-9]{3}\)\s[0-9]{3}-[0-9]{2}-[0-9]{2}/);
-  }, 'Введите Ваш телефон');
-
-  $('.form').each(function(index, el) {
-    $(el).addClass('form-' + index);
-
-    $('.form-' + index).validate({
-      rules: {
-        phone: {
-          required: true,
-          phoneno: true
-        },
-        name: 'required',
-      },
-      messages: {
-        name: 'Введите Ваше имя',
-        phone: 'Введите Ваш телефон',
-      },
-      submitHandler: function(form) {
-        var t = {
-          name: jQuery('.form-' + index).find('input[name=name]').val(),
-          phone: jQuery('.form-' + index).find('input[name=phone]').val(),
-          subject: jQuery('.form-' + index).find('input[name=subject]').val()
-        };
-        ajaxSend('.form-' + index, t);
-      }
-    });
-
-  });
-
-  function ajaxSend(formName, data) {
-    jQuery.ajax({
+  $('form').submit(function() { //Change
+    var th = $(this);
+    $.ajax({
       type: 'POST',
-      url: 'sendmail.php',
-      data: data,
-      success: function() {
-        $('.modal').popup('hide');
-        $('#thanks').popup('show');
-        setTimeout(function() {
-          $(formName).trigger('reset');
-        }, 2000);
-      }
+      url: 'sendmail.php', //Change
+      data: th.serialize()
+    }).done(function() {
+      alert('Спасибо за заявку!');
+      setTimeout(function() {
+        // Done Functions
+        th.trigger('reset');
+      }, 1000);
     });
-  }
+    return false;
+  });
   $('.modal').popup({transition: 'all 0.3s'});
 });
 
